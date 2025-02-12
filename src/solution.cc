6415871154df4171d3f5bea9135f9bb00b9f8d77
Giannis Kostellos <kostellos.giannis@gmail.com>
2025-02-11 21:04:08 -0600

    Update solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -1,6 +1,25 @@
 #include "solution.hpp"
 
 Student ReadStudentRecFromStream(std::istream& is) {
-  // your implementation here...
-  return Student{};
+  std::string firstName, lastName;
+  unsigned int uin;
+  double gpa;
+  char comma;
+  if (!(is >> firstName >> lastName)) {
+    return Student{};
+  }
+  if (lastName.back() != ',') {
+    return Student{};
+  }
+  lastName.pop_back();
+  if (!(is >> uin)) {
+    return Student{};
+  }
+  if (!(is >> comma) || comma != ',') {
+    return Student{};
+  }
+  if (!(is >> gpa)) {
+    return Student{};
+  }
+  return Student{firstName + " " + lastName, uin, gpa};
 }
